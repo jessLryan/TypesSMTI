9767bf17d17d126441877b08186cb9854bfface3
jess <jess.l.ryan@hotmail.com>
2020-04-17 17:19:31 +0100

    fixed some bugs and did some testing on small instances

--- src/main/java/com/mycompany/typessmti/Agent.java
+++ src/main/java/com/mycompany/typessmti/Agent.java
@@ -1,6 +1,6 @@
 package com.mycompany.typessmti;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /*
@@ -13,18 +13,20 @@ import java.util.List;
  * @author jessica
  */
 public class Agent implements Comparable<Agent> {
-
+    //agent number is arbitrary (assigned according to row (for women) or 
+    //column (for men) in the score table
     private int number;
     private List<List<Integer>> prefList;
 
     public Agent(int no) {
         number = no;
     }
-    
+
     public int getNumber() {
         return number;
     }
 
+    //each list in the preference list contains agents in a tie
     public void setPrefList(List<List<Integer>> list) {
         prefList = list;
     }
@@ -33,32 +35,36 @@ public class Agent implements Comparable<Agent> {
         return prefList;
     }
 
+    //agents are compared by preference list - agents who prefer lower-numbered
+    //partners are placed higher in the list (we want to be able to find agents
+    //with identical preference lists)
     @Override
     public int compareTo(Agent a2) {
         List<List<Integer>> prefList2 = a2.getPrefList();
         int prefListLength = prefList.size();
         int h2prefListLength = prefList2.size();
+        iLoop:
         for (int i = 0;; i++) {
-           // System.out.println("i "+i);
-            if (i == prefListLength && h2prefListLength > i ) {
+            if (i == prefListLength && h2prefListLength > i) {
                 return -1;
             }
-            if (i == h2prefListLength && prefListLength > i ) {
+            if (i == h2prefListLength && prefListLength > i) {
                 return 1;
             }
             if (i == h2prefListLength && i == prefListLength) {
                 return 0;
             }
             for (int p = 0;; p++) {
-               // System.out.println("p "+p);
-                if (p == prefList.get(i).size() && prefList2.get(i).size() > p ) {
-                    return -1;
-                }
-                if (p == prefList2.get(i).size() && prefList.get(i).size() > p ) {
+                prefList.get(i).sort(Comparator.comparing(Integer::valueOf));
+                prefList2.get(i).sort(Comparator.comparing(Integer::valueOf));
+                if (p == prefList.get(i).size() && prefList2.get(i).size() > p) {
                     return 1;
                 }
+                if (p == prefList2.get(i).size() && prefList.get(i).size() > p) {
+                    return -1;
+                }
                 if (p == prefList2.get(i).size() && p == prefList.get(i).size()) {
-                    return 0;
+                    continue iLoop;
                 }
                 if (prefList.get(i).get(p) > prefList2.get(i).get(p)) {
                     return 1;
@@ -67,23 +73,22 @@ public class Agent implements Comparable<Agent> {
                     return -1;
                 }
             }
-            
+
         }
-        
+
     }
+
     
-    
-    public boolean areAgentsRankedIdentically(Agent r1,Agent r2) {
-        for (List<Integer> tie:prefList) {
+    //checks whether two agents of the opposite gender are considered equal
+    //partners to this agent (i.e. in a tie)
+    public boolean areAgentsRankedIdentically(Agent r1, Agent r2) {
+        for (List<Integer> tie : prefList) {
             int posAgent1 = tie.indexOf(r1.getNumber());
             int posAgent2 = tie.indexOf(r2.getNumber());
-            if (posAgent1>=0&&posAgent2>=0&&posAgent1==posAgent2) {
+            if (posAgent1 >= 0 && posAgent2 >= 0) {
                 return true;
             }
-            if (posAgent1!=posAgent2) {
-                return false;
-            }
         }
-        return true;
+        return false;
     }
 }
--- src/main/java/com/mycompany/typessmti/TypesCalculator.java
+++ src/main/java/com/mycompany/typessmti/TypesCalculator.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.typessmti;
 
 import java.io.BufferedReader;
@@ -24,70 +20,97 @@ public class TypesCalculator {
     public static void main(String[] args) {
         String input = args[0];
         BufferedReader agentsBufferedReader = getReader(input);
+        //the score table is stored as as a list (rows) of lists (columns)
+        //of triples (row,column,value) where value is the score of this partnership
         List<ArrayList<Triple>> matrix = readToMatrix(agentsBufferedReader);
         List<Agent> men = extractMen(matrix);
         List<Agent> women = extractWomen(matrix);
-        List<Agent> sortedWomen= sortAgents(women);
-        List<Agent> sortedMen= sortAgents(men);
-        List<ArrayList<Agent>> groupedMen = groupAgentsIdenticalPrefs(sortedMen);
-        List<ArrayList<Agent>> groupedWomen = groupAgentsIdenticalPrefs(sortedWomen);
-        List<ArrayList<Agent>> typedMen = findTypes(groupedMen, sortedWomen);
-        List<ArrayList<Agent>> typedWomen = findTypes(groupedWomen, sortedMen);
-        
-        System.out.println("untyped women size " + women.size());
-        System.out.println("typed women size " + typedWomen.size());
-        System.out.println("untyped men size " + men.size());
-        System.out.println("typed men size " + typedMen.size());
+        System.out.println("number of men " + men.size());
+        System.out.println("number of women " + women.size());
+        //sort each gender by pref list (so that agents with identical pref lists 
+        //are grouped in the list)
+        women.sort((o1, o2) -> o1.compareTo(o2));
+        men.sort((o1, o2) -> o1.compareTo(o2));
+        //group each gender into lists of agents with identical preference lists
+        List<ArrayList<Agent>> groupedMen = groupAgentsIdenticalPrefs(men);
+        List<ArrayList<Agent>> groupedWomen = groupAgentsIdenticalPrefs(women);
+//        System.out.println("finding types of men");
+        //from grouped lists, find minimum number of types needed to describe the
+        //instance by checking which agents in a group are considered equal
+        //(are in a tie) in all members of the opposite gender's pref lists
+        List<ArrayList<Agent>> typedMen = findTypes(groupedMen, women);
+//        System.out.println("finding types of women");
+        List<ArrayList<Agent>> typedWomen = findTypes(groupedWomen, men);
+        System.out.println("number of types of men " + typedMen.size());
+        System.out.println("number of types of women " + typedWomen.size());
+//        System.out.println("types of men");
 //        for (ArrayList<Agent> type : typedMen) {
-//            if (type.size() > 1) {
-//                System.out.println("type");
-//                for (Triple<Integer, Agent> pair : type) {
-//                    System.out.println(pair.getKey());
-//                }
+//            System.out.println("type");
+//            for (Agent a : type) {
+//                System.out.println(a.getNumber());
 //            }
 //        }
+//
+//        System.out.println("types of women");
+//        for (ArrayList<Agent> type : typedWomen) {
+//            System.out.println("type");
+//            for (Agent a : type) {
+//                System.out.println(a.getNumber());
+//            }
+//        }
+
     }
+
     
-    
-    
-    public static List<ArrayList<Agent>> findTypes(List<ArrayList<Agent>> groupedAgents, List<Agent> hospitals) {
+    //find minimum number of types needed to descibr the instance
+    public static List<ArrayList<Agent>> findTypes(List<ArrayList<Agent>> groupedAgents, List<Agent> oppositeGenderAgents) {
         List<ArrayList<Agent>> groupedAgentsReturned = new ArrayList<>();
+        //for each group, split the agents in the group into types so that agents
+        //in a type are considered equal by all members of the opposite gender
         for (ArrayList<Agent> group : groupedAgents) {
+//            System.out.println("agents in group");
+//            for (Agent a:group) {
+//                System.out.print(a.getNumber()+" ");
+//            }
+//            System.out.println();
             ArrayList<Agent> groupCopy = (ArrayList<Agent>) group.clone();
+            ArrayList<Agent> type = new ArrayList<>();
+            Agent currentAgent = groupCopy.get(0);
+            type.add(currentAgent);
+            groupCopy.remove(currentAgent);
+            //while there are still agents in the group, find all agents in the
+            //group which are considered equal to the first agent left in the
+            //group, and put these agents in a type
             while (!groupCopy.isEmpty()) {
-                ArrayList<Agent> type = new ArrayList<>();
-                Agent agentOne = groupCopy.get(0);
-                type.add(agentOne);
-                groupCopy.remove(agentOne);
-                for (int i = 0; i < groupCopy.size(); i++) {
-                    Agent agentI = groupCopy.get(i);
-                    if (areAgentsConsideredIdentical(agentOne, agentI, hospitals)) {
-                        type.add(agentI);
-                        groupCopy.remove(agentI);
-                    }
+                Agent agentI = groupCopy.get(0);
+                if (areAgentsConsideredIdentical(currentAgent, agentI, oppositeGenderAgents)) {
+                    type.add(agentI);
+                    groupCopy.remove(agentI);
+                } else {
+                    groupedAgentsReturned.add(type);
+                    type = new ArrayList<>();
+                    type.add(agentI);
+                    groupCopy.remove(agentI);
                 }
-                groupedAgentsReturned.add(type);
             }
+            groupedAgentsReturned.add(type);
         }
         return groupedAgentsReturned;
     }
-    
-    public static boolean areAgentsConsideredIdentical(Agent r1, Agent r2, List<Agent> hospitals) {
-        for (Agent p : hospitals) {
-            if (!p.areAgentsRankedIdentically(r1, r2)) {
+
+    //check if two agents are viewed equally (in a tie) by all agents of the 
+    //opposite gender
+    public static boolean areAgentsConsideredIdentical(Agent r1, Agent r2, List<Agent> agents) {
+        for (Agent a : agents) {
+            if (!a.areAgentsRankedIdentically(r1, r2)) {
                 return false;
             }
         }
         return true;
     }
-    
-    
-    public static List<Agent> sortAgents(List<Agent> unsortedList) {
-        Collections.sort(unsortedList);
-        return unsortedList;
-    }
 
-    
+    //put agents with identical preference lists into groups. The list of agents
+    //given as input is sorted by preference list
     public static List<ArrayList<Agent>> groupAgentsIdenticalPrefs(List<Agent> agents) {
         List<ArrayList<Agent>> groupedAgents = new ArrayList<>();
         ArrayList<Agent> currentList = new ArrayList<>();
@@ -97,7 +120,7 @@ public class TypesCalculator {
         while (pos < agents.size()) {
             Agent newAgent = agents.get(pos);
             if (currentAgent.compareTo(newAgent) == 0) {
-                currentList.add(agents.get(pos));
+                currentList.add(newAgent);
                 currentAgent = newAgent;
             } else {
                 groupedAgents.add(currentList);
@@ -115,83 +138,92 @@ public class TypesCalculator {
         }
         return groupedAgents;
     }
-    
-    
-    
+
+    //each row in the score table corresponds to a woman's preference list 
+    //over the men -> higher score = more preferred partner
     public static List<Agent> extractWomen(List<ArrayList<Triple>> matrix) {
-        List<Agent> women = new ArrayList<Agent>();
+        List<Agent> women = new ArrayList<>();
         for (int row = 0; row < matrix.size(); row++) {
             Agent woman = new Agent(row);
+            //get row of matrix corresponding to this woman's preference list
             List<Triple> rowList = matrix.get(row);
             List<List<Integer>> prefList = new ArrayList<>();
+            //sort the row of the table corresponding to this woman's preference
+            //list so that most preferred men are at the front of the list.
+            
             Collections.sort(rowList, new Comparator<Triple>() {
                 public int compare(Triple o1,
                         Triple o2) {
-                    return Double.valueOf(o1.getValue()).compareTo(o2.getValue());
+                    return Double.valueOf(o2.getValue()).compareTo(o1.getValue());
                 }
             });
-            List<Integer> tie= new ArrayList<>();
+            //Now insert the men into the woman's preference list. Men with equal
+            //scores are placed in a tie in the preference list
+            List<Integer> tie = new ArrayList<>();
             tie.add(rowList.get(0).getColumn());
             double currentVal = rowList.get(0).getValue();
-            for (int i=1;i<rowList.size();i++) {
-                
+            for (int i = 1; i < rowList.size(); i++) {
                 Triple t = rowList.get(i);
-                if (t.getValue()==currentVal) {
+                if (t.getValue() == currentVal) {
                     tie.add(t.getColumn());
-                }
-                else {
+
+                } else {
                     prefList.add(tie);
                     tie = new ArrayList<>();
                     tie.add(t.getColumn());
                 }
+                if (i == rowList.size() - 1) {
+                    prefList.add(tie);
+                }
                 currentVal = rowList.get(i).getValue();
-                
+
             }
             woman.setPrefList(prefList);
             women.add(woman);
         }
         return women;
     }
-
     
-    public static  List<Agent> extractMen(List<ArrayList<Triple>> matrix) {
+    //each column in the score table corresponds to a man's preference list 
+    //over the women -> higher score = more preferred partner
+    public static List<Agent> extractMen(List<ArrayList<Triple>> matrix) {
         List<ArrayList<Triple>> revMatrix = getColumns(matrix);
-        List<Agent> men = new ArrayList<Agent>();
+        List<Agent> men = new ArrayList<>();
         for (int row = 0; row < revMatrix.size(); row++) {
             Agent man = new Agent(row);
-            
             List<Triple> rowList = revMatrix.get(row);
             List<List<Integer>> prefList = new ArrayList<>();
             Collections.sort(rowList, new Comparator<Triple>() {
                 public int compare(Triple o1,
                         Triple o2) {
-                    return Double.valueOf(o1.getValue()).compareTo(Double.valueOf(o2.getValue()));
+                    return Double.valueOf(o2.getValue()).compareTo(o1.getValue());
                 }
             });
-            List<Integer> tie= new ArrayList<>();
-            tie.add(rowList.get(0).getColumn());
+            List<Integer> tie = new ArrayList<>();
+            tie.add(rowList.get(0).getRow());
             double currentVal = rowList.get(0).getValue();
-            for (int i=1;i<rowList.size();i++) {
-                
+            for (int i = 1; i < rowList.size(); i++) {
                 Triple t = rowList.get(i);
-                if (t.getValue()==currentVal) {
-                    tie.add(t.getColumn());
-                }
-                else {
+                if (t.getValue() == currentVal) {
+                    tie.add(t.getRow());
+
+                } else {
                     prefList.add(tie);
                     tie = new ArrayList<>();
-                    tie.add(t.getColumn());
+                    tie.add(t.getRow());
+                }
+                if (i == rowList.size() - 1) {
+                    prefList.add(tie);
                 }
                 currentVal = rowList.get(i).getValue();
-                
             }
             man.setPrefList(prefList);
             men.add(man);
         }
         return men;
     }
-    
-     public static List<ArrayList<Triple>> getColumns(List<ArrayList<Triple>> matrix) {
+
+    public static List<ArrayList<Triple>> getColumns(List<ArrayList<Triple>> matrix) {
         int rows = matrix.size();
         int columns = matrix.get(0).size();
         List<ArrayList<Triple>> newMatrix = new ArrayList<>();
@@ -204,8 +236,8 @@ public class TypesCalculator {
         }
         return newMatrix;
     }
-    
 
+    //convert text file containing table of scores into matrix
     public static List<ArrayList<Triple>> readToMatrix(BufferedReader reader) {
         List<ArrayList<Triple>> listOfLists = new ArrayList<>();
         try {
@@ -216,10 +248,10 @@ public class TypesCalculator {
             while ((line = reader.readLine()) != null) {
                 ArrayList<Triple> list = new ArrayList<>();
                 String[] splitArray = line.trim().split("\\s+");
-                
+
                 for (int col = 0; col < splitArray.length; col++) {
                     double value = Double.parseDouble(splitArray[col]);
-                    list.add(new Triple(row,col,value));
+                    list.add(new Triple(row, col, value));
                 }
                 listOfLists.add(list);
                 row++;
